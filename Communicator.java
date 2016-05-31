dfd808ecc6bc48da82dacb2bcb44a837a83eed63
vaibhavcrazy <haither1991@gmail.com>
2016-05-31 16:40:45 +0530

    Create Communicator.java

---
+++ Communicator.java
@@ -0,0 +1,10 @@
+package ilp.tcs.com.myapplication;
+
+/**
+ * Created by 1014085 on 2/2/2016.
+ */
+public interface Communicator {
+
+    public void respond(ListBean listBean);
+    public void respond(NewsBean newsBean);
+}
