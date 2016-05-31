af5d8221dc5931ae588646b731669bd64d64510e
vaibhavcrazy <haither1991@gmail.com>
2016-05-31 16:43:53 +0530

    Create ListBean.java

---
+++ ListBean.java
@@ -0,0 +1,27 @@
+package ilp.tcs.com.myapplication;
+
+/**
+ * Created by 1014085 on 2/2/2016.
+ */
+public class ListBean {
+    String title;
+    int image;
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+}
